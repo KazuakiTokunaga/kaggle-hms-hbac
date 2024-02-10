07f16e61d04d355f3b37e2745e4ad2451f0652da
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-02-10 23:52:40 +0900

    update

--- colab-work-ktoku/utils.py
+++ colab-work-ktoku/utils.py
@@ -16,7 +16,8 @@ class Logger:
         self.general_logger = logging.getLogger('general')
         stream_handler = logging.StreamHandler()
         self.general_logger.propagate = False
-        file_general_handler = logging.FileHandler(f'{log_path}general_{filename_suffix}.log')
+        if not debug:
+            file_general_handler = logging.FileHandler(f'{log_path}general_{filename_suffix}.log')
         for h in self.general_logger.handlers[:]:
             self.general_logger.removeHandler(h)
             h.close()
