280c2e9f00675f79116dcfee24e8ca039ed4bfee
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-02-29 23:17:27 +0900

    update

--- exp-work-ktoku/exp001.py
+++ exp-work-ktoku/exp001.py
@@ -129,9 +129,9 @@ class HMSDataset(Dataset):
             # CROP TO 256 TIME STEPS
             X[14:-14,:,k] = img[:,22:-22] / 2.0
 
-        # Chris
-        img = self.specs['chris'][row.eeg_id] # (128, 256, 4)
-        X[:,:,4:8] = img
+        # # Chris
+        # img = self.specs['chris'][row.eeg_id] # (128, 256, 4)
+        # X[:,:,4:8] = img
 
         # # v2
         # img = self.specs['v2'][row.eeg_id] # (128, 256, 4)
@@ -184,14 +184,14 @@ class CustomInputTransform(nn.Module):
         super(CustomInputTransform, self).__init__()
 
     def forward(self, x): 
-        x1 = torch.cat([x[:, :, :, i:i+1] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
-        x2 = torch.cat([x[:, :, :, i+4:i+5] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
+        x =  torch.cat([x[:, :, :, i:i+1] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
+        # x2 = torch.cat([x[:, :, :, i+4:i+5] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
         # x3 = torch.cat([x[:, :, :, i+8:i+9] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
         # x4 = torch.cat([x[:, :, :, i+12:i+13] for i in range(4)], dim=1) # (batch_size, 512, 256, 1)
         # x5 = torch.cat([x[:, :, :, i+16:i+17] for i in range(2)], dim=1) # (batch_size, 256, 256, 1)
 
         # x_t = torch.cat([x1, x2, x3], dim=2) # (batch_size, 512, 768, 1)
-        x = torch.cat([x1, x2], dim=2) # (batch_size, 512, 768, 1)
+        # x = torch.cat([x1, x2], dim=2) # (batch_size, 512, 768, 1)
         # x_t2 = torch.cat([x4, x5], dim=1) #(batch_size, 768, 256, 1)
         # x_t2 = x_t2.permute(0, 2, 1, 3) # (batch_size, 256, 768, 1)
         # x = torch.cat([x_t, x_t2], dim=1) # (batch_size, 768, 768, 1)
@@ -418,7 +418,7 @@ class Runner():
 
         # READ ALL SPECTROGRAMS
         self.all_spectrograms = {}
-        for name in ['kaggle', 'chris']:
+        for name in ['kaggle']:
             logger.info(f'Loading spectrograms eeg_spec_{name}.py')
             self.all_spectrograms[name] = np.load(ROOT_PATH + f'/input/hms-hbac-data/eeg_specs_{name}.npy',allow_pickle=True).item()
 
