2e897090d467dc351d73cd07fde91cde5c7b5d0d
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-03-20 14:41:34 +0900

    update

--- exp-work-ktoku/exp001.py
+++ exp-work-ktoku/exp001.py
@@ -42,7 +42,7 @@ class RCFG:
     SHEET_KEY = '1Wcg2EvlDgjo0nC-qbHma1LSEAY_OlS50mJ-yI4QI-yg'
     PSEUDO_LABELLING = False
     LABELS_V2 = True
-    USE_SPECTROGRAMS = ['kaggle', 'cwt_cmor_v67', 'cwt_cmor_10sec_v67']
+    USE_SPECTROGRAMS = ['kaggle', 'cwt_cmor_v69', 'cwt_cmor_10sec_v69']
     CREATE_SPECS = True
     USE_ALL_LOW_QUALITY = False
     ADD_MIXUP_DATA = False
