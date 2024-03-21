9a2da3b5d9a402bbcc489dddfeee84e1a4475a48
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-03-21 16:15:13 +0900

    update

--- exp-work-ktoku/exp001.py
+++ exp-work-ktoku/exp001.py
@@ -43,8 +43,8 @@ class RCFG:
     SHEET_KEY = '1Wcg2EvlDgjo0nC-qbHma1LSEAY_OlS50mJ-yI4QI-yg'
     PSEUDO_LABELLING = False
     LABELS_V2 = True
-    USE_SPECTROGRAMS = ['kaggle']
-    # USE_SPECTROGRAMS = ['kaggle', 'cwt_cmor_v67', 'cwt_cmor_10sec_v67']
+    # USE_SPECTROGRAMS = ['kaggle']
+    USE_SPECTROGRAMS = ['kaggle', 'cwt_cmor_v67', 'cwt_cmor_10sec_v67']
     CREATE_SPECS = True
     USE_ALL_LOW_QUALITY = False
     ADD_MIXUP_DATA = False
@@ -156,20 +156,20 @@ class HMSDataset(Dataset):
         # img = np.vstack((img[:, :256, :], img[:, 256:, :])) # (64, 512, 4) -> (128, 256, 4)に変換
         # x2 = np.concatenate([img[:, :, i:i+1] for i in range(4)], axis=0) # (512, 256, 1)
 
+        # (64, 512, 4)型
+        img = self.specs['cwt_cmor_v67'][row.eeg_id] # (64, 512, 4)
+        img = np.concatenate([img[:, :, i:i+1] for i in range(4)], axis=0) # (256, 512, 1)
+        x2 = img.transpose(1, 0, 2) # (512, 256, 1)
+
         # # (64, 512, 4)型
-        # img = self.specs['cwt_cmor_v67'][row.eeg_id] # (64, 512, 4)
-        # img = np.concatenate([img[:, :, i:i+1] for i in range(4)], axis=0) # (256, 512, 1)
-        # x2 = img.transpose(1, 0, 2) # (512, 256, 1)
+        img = self.specs['cwt_cmor_10sec_v67'][row.eeg_id] # (64, 512, 4)
+        img = np.concatenate([img[:, :, i:i+1] for i in range(4)], axis=0) # (256, 512, 1)
+        x3 = img.transpose(1, 0, 2) # (512, 256, 1)
 
-        # # # (64, 512, 4)型
-        # img = self.specs['cwt_cmor_10sec_v67'][row.eeg_id] # (64, 512, 4)
-        # img = np.concatenate([img[:, :, i:i+1] for i in range(4)], axis=0) # (256, 512, 1)
-        # x3 = img.transpose(1, 0, 2) # (512, 256, 1)
+        X = np.concatenate([x1, x2, x3], axis=1) # (512, 768, 1)
 
-        # X = np.concatenate([x1, x2, x3], axis=1) # (512, 768, 1)
-
-        # return X, y # (), (6)
-        return x1, y
+        return X, y # (), (6)
+        # return x1, y
 
     def _augment_batch(self, img):
         transforms = A.Compose([
