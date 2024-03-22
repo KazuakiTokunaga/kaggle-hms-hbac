e0fa71863f79eccead6033f484e2902df175706f
Kazuaki Tokunaga <32542500+KazuakiTokunaga@users.noreply.github.com>
2024-03-22 09:20:06 +0900

    update

--- exp-work-ktoku/exp001.py
+++ exp-work-ktoku/exp001.py
@@ -190,7 +190,7 @@ class GeM(nn.Module):
     def __init__(self, p=3, eps=1e-6, device='cuda:0'):
         super(GeM, self).__init__()
         # pを固定値として定義
-        self.p = Parameter(torch.ones(1, device=device) * p)
+        self.p = torch.ones(1, device=device) * p
         self.eps = eps
 
     def forward(self, x):
